c351f7192460c9745c6197fb1fc254c0793f512e
jana <jana.abusaa@gmail.com>
2024-01-02 19:05:32 +0200

    Initial commit

---
+++ app.py
@@ -0,0 +1,40 @@
+from flask import Flask, request, render_template
+from sklearn.feature_extraction.text import TfidfVectorizer
+import pickle
+import catboost
+
+app = Flask(__name__)
+
+model = pickle.load(open('models/mylatestmodel.pkl', 'rb'))
+vectorizer = pickle.load(open('models/vectorizer.pkl', 'rb'))
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+
+@app.route('/predict', methods=['POST'])
+def predict():
+    email = request.form.values()
+
+    alphabet = '''abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'''
+    text = ""
+
+    for element in email:
+        if element in alphabet:
+            text += element
+
+    text = vectorizer.transform([text])
+
+    prediction = model.predict(text)
+
+    if prediction == 0:
+        return render_template('index.html', prediction_text='This email is a HAM!')
+    else:
+        return render_template('index.html', prediction_text='This email is a SPAM!')
+
+
+if __name__ == '__main__':
+    app.debug = True
+    app.run()
+
